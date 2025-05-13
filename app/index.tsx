0360bc697feaa557a5415a68d9e98f7a8951032a
Samed OLMUS <samedolmus7@gmail.com>
2025-05-13 17:56:15 +0300

    feat: add QR code scanning functionality
    - Implemented QR code scanner in the app using react-native-qrcode-scanner.
    - Added a button to toggle the scanner visibility.
    - Displayed scanned data in an alert.
    - Updated package.json and package-lock.json to include necessary dependencies for QR code scanning and permissions.
    - Changed project name from "Warehouse" to "warehouse" for consistency.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,58 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Alert, Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default function Index() {
+  const [isScannerVisible, setScannerVisible] = useState(false);
+
+  const handleScannerToggle = () => {
+    setScannerVisible(!isScannerVisible);
+  };
+
+  const handleScanResult = (event: { data: string }) => {
+    Alert.alert('Scanned Data', event.data ?? 'No data found');
+    setScannerVisible(false);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="Scan QR Code" onPress={handleScannerToggle} />
+      {isScannerVisible && (
+        <QRCodeScanner
+          onRead={handleScanResult}
+          topContent={
+            <Text style={styles.centerText}>
+              Scan a QR code
+            </Text>
+          }
+          bottomContent={
+            <TouchableOpacity style={styles.buttonTouchable} onPress={handleScannerToggle}>
+              <Text style={styles.buttonText}>OK</Text>
+            </TouchableOpacity>
+          }
+        />
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+    textAlign: 'center',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
